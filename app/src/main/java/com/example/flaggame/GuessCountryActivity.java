e985842952dd10c420b8400b5a409662e7c4de98
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2024-10-17 01:53:22 +0100

    GuessCountryActivity flags randomly iterate each click of submit/next. Correct answers aren't registering

--- app/src/main/java/com/example/flaggame/GuessCountryActivity.java
+++ app/src/main/java/com/example/flaggame/GuessCountryActivity.java
@@ -1,8 +1,15 @@
 package com.example.flaggame;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,19 +17,116 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 public class GuessCountryActivity extends AppCompatActivity {
+
+    private ImageView flagImage;
+    private HashMap<Bitmap, String> flagBitmap;
+    private List<Bitmap> flagList;
+    private String rightCountry;
+    private TextView resultText;
+    private Button submitBtn;
+    private boolean isNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess_country);
 
+        flagImage = findViewById(R.id.imageView);
+        submitBtn = findViewById(R.id.submitBtn);
         Spinner countriesSpinner = findViewById(R.id.countriesSpinner);
+        resultText = findViewById(R.id.resultText);
+
+        // initialise hashmap
+        flagBitmap = new HashMap<>();
+        // load bitmaps and store them
+        flagBitmap.put(loadBitmap(R.drawable.algeria), "Algeria");
+        flagBitmap.put(loadBitmap(R.drawable.australia), "Australia");
+        flagBitmap.put(loadBitmap(R.drawable.belgium), "Belgium");
+        flagBitmap.put(loadBitmap(R.drawable.brazil), "Brazil");
+        flagBitmap.put(loadBitmap(R.drawable.canada), "Canada");
+        flagBitmap.put(loadBitmap(R.drawable.colombia), "Colombia");
+        flagBitmap.put(loadBitmap(R.drawable.croatia), "Croatia");
+        flagBitmap.put(loadBitmap(R.drawable.denmark), "Denmark");
+        flagBitmap.put(loadBitmap(R.drawable.egypt), "Egypt");
+        flagBitmap.put(loadBitmap(R.drawable.finland), "Finland");
+        flagBitmap.put(loadBitmap(R.drawable.france), "France");
+        flagBitmap.put(loadBitmap(R.drawable.germany), "Germany");
+        flagBitmap.put(loadBitmap(R.drawable.greece), "Greece");
+        flagBitmap.put(loadBitmap(R.drawable.portugal), "Portugal");
+        flagBitmap.put(loadBitmap(R.drawable.singapore), "Singapore");
+        flagBitmap.put(loadBitmap(R.drawable.spain), "Spain");
+        flagBitmap.put(loadBitmap(R.drawable.sweden), "Sweden");
+        flagBitmap.put(loadBitmap(R.drawable.switzerland), "Switzerland");
+        flagBitmap.put(loadBitmap(R.drawable.turkey), "Turkey");
+        flagBitmap.put(loadBitmap(R.drawable.ukraine), "Ukraine");
+        flagBitmap.put(loadBitmap(R.drawable.zimbabwe), "Zimbabwe");
+
+        // list of bitmaps to randomise
+        flagList = new ArrayList<>(flagBitmap.keySet());
+
+        // show random flag when activity loads
+        showRandomFlag();
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.
                 createFromResource(this, R.array.country_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         countriesSpinner.setAdapter(adapter);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isNext) {
+                    showRandomFlag();
+                    isNext = false;  // reset to submit
+                } else {
+                    String chosenCountry = countriesSpinner.getSelectedItem().toString();
+                    checkAnswer(chosenCountry);
+                }
+            }
+        });
+
+        }
+
+        private Bitmap loadBitmap(int i) {
+            // loads image as bitmap with BitmapFactory
+            return BitmapFactory.decodeResource(getResources(), i);
+        }
+
+        private void showRandomFlag() {
+        // chooses random bitmap from list
+        Random random = new Random();
+        Bitmap randomFlag = flagList.get(random.nextInt(flagList.size()));
+
+        flagImage.setImageBitmap(randomFlag);   // sets imageview to random flag
+
+        rightCountry = flagBitmap.get(randomFlag);
+
+        // resets result msg
+        resultText.setText("");
+        submitBtn.setText("Submit");
+        isNext = false;
+        }
+
+        private void checkAnswer(String chosenCountry) {
+
+        if (chosenCountry == rightCountry) {
+            resultText.setText("Correct!");
+            resultText.setTextColor(Color.GREEN);
+        } else {
+            resultText.setText("Wrong! The right answer is: " + rightCountry);
+            resultText.setTextColor(Color.RED);
+        }
+
+        submitBtn.setText("Next");
+        isNext = true;
         }
     }
